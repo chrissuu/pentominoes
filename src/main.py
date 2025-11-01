2aa4370383010be2f124a97750273f2ae734de45
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-10-31 20:54:25 -0400

    Generalize pentomino class to all polyominoes

--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
-from pysat.solvers import Glucose3
-from pysat.formula import CNF
-from typing import List, Tuple
 import itertools
-from Pentomino import *
 from copy import deepcopy
 
-class PentominoSolver:
+from pysat.formula import CNF
+from pysat.solvers import Glucose3
+
+from polyomino import *
+
+
+class PolyominoSolver:
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
-        self.pentominoes = [F(), I(), L(), N(), P(), T(), U(), V(), W(), X(), Y(), Z()]
+        self.polyominoes = [
+            F5(),
+            I5(),
+            L5(),
+            N5(),
+            P5(),
+            T5(),
+            U5(),
+            V5(),
+            W5(),
+            X5(),
+            Y5(),
+            Z5(),
+        ]
 
         self.var_counter = 1
         self.p_vars = {}
@@ -22,22 +37,24 @@ class PentominoSolver:
             self.var_counter += 1
         return self.p_vars[key]
 
-    def get_pentomino_tiles(self, pentomino_idx: int, x: int, y: int, rotation: int) -> List[Tuple[int, int]]:
-        pentomino = self.pentominoes[pentomino_idx]
-        pentomino_copy: Pentomino = deepcopy(pentomino).rotate(rotation)
-        tiles = [(x + tx, y + ty) for (tx, ty) in pentomino_copy.tiles]
+    def get_polyomino_tiles(
+        self, polyomino_idx: int, x: int, y: int, rotation: int
+    ) -> List[Tuple[int, int]]:
+        polyomino = self.polyominoes[polyomino_idx]
+        polyomino_copy: Polyomino = deepcopy(polyomino).rotate(rotation)
+        tiles = [(x + tx, y + ty) for (tx, ty) in polyomino_copy.tiles]
         return tiles
 
     def is_valid_placement(self, tiles: List[Tuple[int, int]]) -> bool:
         return all(0 <= tx < self.width and 0 <= ty < self.height for tx, ty in tiles)
 
-    def exactly_one_pentomino_constraint(self):
-        for i in range(len(self.pentominoes)):
+    def exactly_one_polyomino_constraint(self):
+        for i in range(len(self.polyominoes)):
             placements = []
             for x in range(self.width):
                 for y in range(self.height):
                     for r in range(4):
-                        tiles = self.get_pentomino_tiles(i, x, y, r)
+                        tiles = self.get_polyomino_tiles(i, x, y, r)
                         if self.is_valid_placement(tiles):
                             placements.append(self.get_p_var(x, y, r, i))
 
@@ -51,11 +68,11 @@ class PentominoSolver:
             for ty in range(self.height):
                 occupiers = []
 
-                for i in range(len(self.pentominoes)):
+                for i in range(len(self.polyominoes)):
                     for x in range(self.width):
                         for y in range(self.height):
                             for r in range(4):
-                                tiles = self.get_pentomino_tiles(i, x, y, r)
+                                tiles = self.get_polyomino_tiles(i, x, y, r)
                                 if self.is_valid_placement(tiles) and (tx, ty) in tiles:
                                     occupiers.append(self.get_p_var(x, y, r, i))
 
@@ -63,7 +80,7 @@ class PentominoSolver:
                     self.cnf.append([-o1, -o2])
 
     def solve(self) -> bool:
-        self.exactly_one_pentomino_constraint()
+        self.exactly_one_polyomino_constraint()
         self.add_no_overlap_constraints()
 
         solver = Glucose3()
@@ -85,14 +102,14 @@ class PentominoSolver:
 
     def get_board(self) -> List[List[str]]:
         placements = self.get_placements()
-        grid = [[' ' for _ in range(self.width)] for _ in range(self.height)]
+        grid = [[" " for _ in range(self.width)] for _ in range(self.height)]
 
         for x, y, r, i in placements:
-            tiles = self.get_pentomino_tiles(i, x, y, r)
-            pentomino_name = self.pentominoes[i].name
+            tiles = self.get_polyomino_tiles(i, x, y, r)
+            polyomino_name = self.polyominoes[i].name
             for tx, ty in tiles:
                 if 0 <= tx < self.width and 0 <= ty < self.height:
-                    grid[ty][tx] = pentomino_name
+                    grid[ty][tx] = polyomino_name
 
         return grid
 
@@ -103,7 +120,8 @@ class PentominoSolver:
             print("|" + "".join(row) + "|")
         print("+" + "-" * self.width + "+")
 
-solver = PentominoSolver(20, 20)
+
+solver = PolyominoSolver(20, 20)
 
 if solver.solve():
     print("Solution found!")
--- src/polyomino.py
+++ src/polyomino.py
@@ -2,39 +2,41 @@ from typing import List, Tuple
 
 Tile = Tuple[int, int]
 
-class Pentomino:
+
+class Polyomino:
     """
-    Base Pentomino Class
-    
-    Uses the first naming convention found in
+    Base Polyomino Class
+
+    For pentominoes, uses the first naming convention found in
     the pentomino wiki page: https://en.wikipedia.org/wiki/Pentomino
     """
+
     name = None
     default_tiles: List[Tile] = []
 
     def __init__(self):
         self.tiles = self.default_tiles
 
-    def rotate(self, k: int) -> 'Pentomino':
+    def rotate(self, k: int) -> "Polyomino":
         k = k % 4
         for _ in range(k):
             self.tiles = [(y, -x) for (x, y) in self.tiles]
         return self
 
-    def reflect_horizontally(self) -> 'Pentomino':
+    def reflect_horizontally(self) -> "Polyomino":
         self.tiles = [(-x, y) for (x, y) in self.tiles]
         return self
 
-    def reflect_vertically(self) -> 'Pentomino':
+    def reflect_vertically(self) -> "Polyomino":
         self.rotate(2)
         self.reflect_horizontally()
         return self
-    
-    def recenter(self, new_center: Tile) -> 'Pentomino':
+
+    def recenter(self, new_center: Tile) -> "Polyomino":
         cx, cy = new_center
         self.tiles = [(x - cx, y - cy) for (x, y) in self.tiles]
         return self
-    
+
     def print_in_grid(self, size: int = 7):
         grid = [[" " for _ in range(size)] for _ in range(size)]
 
@@ -47,17 +49,20 @@ class Pentomino:
 
         for row in reversed(grid):
             print(" ".join(row))
-    
-class F(Pentomino):
+
+
+class F5(Polyomino):
     """
       o o
     x o
       o
     """
+
     name = "F"
     default_tiles = [(0, 0), (1, 0), (1, -1), (1, 1), (2, 1)]
 
-class I(Pentomino):
+
+class I5(Polyomino):
     """
     o
     o
@@ -65,97 +70,118 @@ class I(Pentomino):
     o
     x
     """
+
     name = "I"
     default_tiles = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)]
 
-class L(Pentomino):
+
+class L5(Polyomino):
     """
     o
     o
     o
     x o
     """
+
     name = "L"
     default_tiles = [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0)]
 
-class N(Pentomino):
+
+class N5(Polyomino):
     """
       o
       o
     o o
     x
     """
+
     name = "N"
     default_tiles = [(0, 0), (0, 1), (1, 1), (1, 2), (1, 3)]
 
-class P(Pentomino):
+
+class P5(Polyomino):
     """
     o o
     o o
     x
     """
+
     name = "P"
     default_tiles = [(0, 0), (0, 1), (0, 2), (1, 1), (1, 2)]
 
-class T(Pentomino):
+
+class T5(Polyomino):
     """
     o o o
       o
       x
     """
+
     name = "T"
     default_tiles = [(0, 0), (0, 1), (0, 2), (-1, 2), (1, 2)]
 
-class U(Pentomino):
+
+class U5(Polyomino):
     """
     o   o
     x o o
     """
+
     name = "U"
     default_tiles = [(0, 0), (0, 1), (1, 0), (2, 0), (2, 1)]
 
-class V(Pentomino):
+
+class V5(Polyomino):
     """
         o
         o
     x o o
     """
+
     name = "V"
     default_tiles = [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]
 
-class W(Pentomino):
+
+class W5(Polyomino):
     """
         o
       o o
     x o
     """
+
     name = "W"
     default_tiles = [(0, 0), (1, 0), (1, 1), (2, 1), (2, 2)]
 
-class X(Pentomino):
+
+class X5(Polyomino):
     """
       o
     o o o
       x
     """
+
     name = "X"
     default_tiles = [(0, 0), (0, 1), (-1, 1), (1, 1), (0, 2)]
 
-class Y(Pentomino):
+
+class Y5(Polyomino):
     """
       o
     o o
       o
       x
     """
+
     name = "Y"
     default_tiles = [(0, 0), (0, 1), (0, 2), (-1, 2), (0, 3)]
 
-class Z(Pentomino):
+
+class Z5(Polyomino):
     """
     o o
       o
       x o
     """
+
     name = "Z"
     default_tiles = [(0, 0), (1, 0), (0, 1), (0, 2), (-1, 2)]
